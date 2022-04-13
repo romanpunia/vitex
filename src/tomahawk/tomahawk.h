89b3c9a0dd85fea4854716f4738e0b76c55093e3
romanpunia <roman_punia@bk.ru>
2022-04-13 23:32:20 +0300

    Version update

--- src/tomahawk/tomahawk.h
+++ src/tomahawk/tomahawk.h
@@ -1,8 +1,8 @@
 #ifndef TOMAHAWK_H
 #define TOMAHAWK_H
-#define TH_MAJOR_VERSION 5
-#define TH_MINOR_VERSION 4
-#define TH_PATCH_LEVEL 21
+#define TH_MAJOR_VERSION 6
+#define TH_MINOR_VERSION 5
+#define TH_PATCH_LEVEL 22
 #define TH_VERSION(X, Y, Z) ((X) * 1000 + (Y) * 100 + (Z))
 #define TH_AT_LEAST(X, Y, Z) (TH_VERSION(TH_MAJOR_VERSION, TH_MINOR_VERSION, TH_PATCH_LEVEL) >= TH_VERSION(X, Y, Z))
 #include "core/core.h"
